d59f2ad98c0617f47f5b7925e40093a4dbf7e2bc
Christian Gogolin <git@cgogolin.de>
2018-08-07 13:55:18 +0200

    clean up of latex related tests

--- tests/o3po-latex-test.php
+++ tests/o3po-latex-test.php
@@ -5,75 +5,98 @@ require_once dirname( __FILE__ ) . '/../o3po/includes/class-o3po-latex.php';
 class O3PO_LatexTest extends PHPUnit_Framework_TestCase
 {
 
- 
+
     public function test_get_bbl_file() {
 
-        return file_get_contents(dirname( __FILE__ ) . "/resources/test_bibliography.bbl");
+        $content = file_get_contents(dirname( __FILE__ ) . "/resources/test_bibliography.bbl");
+        $this->assertNotEmpty($content);
+
+        return $content;
     }
 
     public function test_get_biblatex_bbl_file() {
 
-        return file_get_contents(dirname( __FILE__ ) . "/resources/test_bibliography_biblatex.bbl");
+        $content = file_get_contents(dirname( __FILE__ ) . "/resources/test_bibliography_biblatex.bbl");
+
+        $this->assertNotEmpty($content);
+
+        return $content;
     }
 
     public function test_get_latex_file() {
 
-        return file_get_contents(dirname( __FILE__ ) . "/resources/latex.tex");
+        $content = file_get_contents(dirname( __FILE__ ) . "/resources/latex.tex");
+        $this->assertNotEmpty($content);
+
+        return $content;
     }
-    
+
         /**
          * @depends test_get_bbl_file
-         */ 
+         */
     public function test_extract_latex_macros_bbl( $bbl ) {
 
-        return O3PO_Latex::extract_latex_macros($bbl);
+        $macros = O3PO_Latex::extract_latex_macros($bbl);
+        $this->assertEquals(count($macros), 31);
+
+        return $macros;
     }
 
         /**
          * @depends test_get_biblatex_bbl_file
-         */ 
+         */
     public function test_extract_latex_macros_bbl_biblatex( $bbl ) {
 
-        return O3PO_Latex::extract_latex_macros($bbl);
+        $macros = O3PO_Latex::extract_latex_macros($bbl);
+        $this->assertEquals(count($macros), 0);
+        return $macros;
     }
 
         /**
          * @depends test_extract_latex_macros_bbl
-         */ 
+         */
     public function test_remove_special_macros_to_ignore_in_bbl( $latex_macro_definitions ) {
 
-        return O3PO_Latex::remove_special_macros_to_ignore_in_bbl($latex_macro_definitions);
+        $macros = O3PO_Latex::remove_special_macros_to_ignore_in_bbl($latex_macro_definitions);
+        $this->assertEquals(count($macros), 29);
+        return $macros;
     }
 
         /**
          * @depends test_remove_special_macros_to_ignore_in_bbl
-         */ 
+         */
     public function test_all_special_macros_removed( $latex_macro_definitions_without_specials ) {
 
         $special_macros_to_ignore = O3PO_Latex::get_special_macros_to_ignore_in_bbl();
-                
+
         $this->assertTrue(!empty($latex_macro_definitions_without_specials));
         $this->assertTrue(!empty($special_macros_to_ignore));
 
         foreach($latex_macro_definitions_without_specials as $latex_macro_definition)
             $this->assertNotContains($latex_macro_definition[2], $special_macros_to_ignore);
-        
+
     }
-    
+
         /**
          * @depends test_get_bbl_file
-         */ 
+         */
     public function test_parse_bbl( $bbl ) {
 
-        return O3PO_Latex::parse_bbl($bbl);
+        $parsed_bbl = O3PO_Latex::parse_bbl($bbl);
+        $this->assertEquals(count($parsed_bbl), 64);
+
+        return $parsed_bbl;
     }
 
         /**
          * @depends test_get_biblatex_bbl_file
-         */ 
+         */
     public function test_parse_biblatex_bbl( $bbl ) {
 
-        return O3PO_Latex::parse_bbl($bbl);
+        $parsed_bbl = O3PO_Latex::parse_bbl($bbl);
+        $this->assertEquals(count($parsed_bbl), 294);
+
+        return $parsed_bbl;
     }
 
     public function get_month_string_provider() {
@@ -94,7 +117,7 @@ class O3PO_LatexTest extends PHPUnit_Framework_TestCase
             [13 , null],
         ];
     }
-    
+
 
         /**
          * @dataProvider get_month_string_provider
@@ -108,62 +131,40 @@ class O3PO_LatexTest extends PHPUnit_Framework_TestCase
             $exception = $e;
         }
 
-        if($expected!=null and $exception!=null)
-            throw($e);
-        
-        if($expected===null and $exception===null)
-            throw(new Exception("get_month_string() should throw an exception for input " . $month));
+        if($expected!=null)
+            $this->assertNull($exception);
+        if($expected===null)
+            $this->assertNotNull($exception);
+
     }
-    
-    
-        /**
-         * @depends test_extract_latex_macros_bbl
-         */ 
-    public function test_all_makros_were_caught_bbl( $latex_macro_definitions ) {
-
-        $this->assertCount(31, $latex_macro_definitions);
-    }
-
-        /**
-         * @depends test_extract_latex_macros_bbl_biblatex
-         */ 
-    public function test_all_makros_were_caught_bbl_biblatex( $latex_macro_definitions ) {
-
-        $this->assertCount(0, $latex_macro_definitions);
-    }
-
 
         /**
          * @depends test_get_latex_file
-         */ 
+         */
     public function test_extract_latex_macros_latex( $latex ) {
-        
-        return O3PO_Latex::extract_latex_macros($latex);;
+
+        $macros = O3PO_Latex::extract_latex_macros($latex);;
+        $this->assertEquals(count($macros), 68);
+
+        return $macros;
     }
 
         /**
          * @depends test_extract_latex_macros_latex
-         */ 
-    public function test_all_makros_were_caught_latex_biblatex( $latex_macro_definitions ) {
-        $this->assertCount(68, $latex_macro_definitions);
-    }
-    
-        /**
-         * @depends test_extract_latex_macros_latex
          * @depends test_get_latex_file
-         */ 
+         */
     public function test_expand_latex_macros_latex( $macro_definitions, $text ) {
-        
+
         $text_expanded = O3PO_Latex::expand_latex_macros( $macro_definitions, $text );
-        
+
         $text_doubl_expanded = O3PO_Latex::expand_latex_macros($macro_definitions, $text_expanded);
-        
+
         $this->assertNotSame($text, $text_expanded);
         $this->assertSame($text_doubl_expanded, $text_expanded);
     }
 
 
-    
+
     public function latex_to_utf8_outside_math_mode_test_case_provider() {
         return [
             ["\\'  \n a" , "á"],
@@ -196,9 +197,9 @@ class O3PO_LatexTest extends PHPUnit_Framework_TestCase
             ["\\`{\\a" , "\\`{\\a"],
             ["\\`a}" , "à}"],
             ['\\"{a}' , 'ä'],
-            ['\\"  
+            ['\\"
 a' , 'ä'],
-            ['\\"  
+            ['\\"
 ab' , 'äb'],
             ['\\"{   a}' , 'ä'],
             ['\\"{   a  }' , 'ä'],
@@ -212,7 +213,7 @@ ab' , 'äb'],
             ['\\"xax ' , '\\"xax '],
             ['\\"{a' , '\\"{a'],
             ['\\"{\\a' , '\\"{\\a'],
-            ['\\"a}' , 'ä}'],    
+            ['\\"a}' , 'ä}'],
             ['a\\ss b' , 'aßb'],
             ['a\\ss {}b' , 'aßb'],
             ['a\\ss {} b' , 'aß b'],
@@ -226,16 +227,16 @@ ab' , 'äb'],
             ['\\vemph ' , '\\vemph '],
             ['\\vca' , '\\vca'],
             ['\\vc,' , '\\vc,'],
-            ['\\v\\c' , 'č'],            
+            ['\\v\\c' , 'č'],
                 ];
     }
-    
+
         /**
          * @dataProvider latex_to_utf8_outside_math_mode_test_case_provider
          */
     public function test_latex_to_utf8_outside_math_mode( $input, $expected ) {
         $this->assertSame($expected, O3PO_Latex::latex_to_utf8_outside_math_mode($input, false));
     }
-    
-    
+
+
 }
