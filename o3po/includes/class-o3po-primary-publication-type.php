974c732b2513643d760bc3ec45182c7f9c467ba5
Johannes Drever <drever@lrz.uni-muenchen.de>
2018-09-01 19:50:41 +0200

    Fix tests

--- o3po/includes/class-o3po-primary-publication-type.php
+++ o3po/includes/class-o3po-primary-publication-type.php
@@ -470,7 +470,7 @@ class O3PO_PrimaryPublicationType extends O3PO_PublicationType {
 
         $subject  = ($this->environment->is_test_environment() ? 'TEST ' : '').
                     O3PO_EmailTemplates::self_notification_subject(
-                              $settings->get_plugin_option('self_notification_subject_template').
+                              $settings->get_plugin_option('self_notification_subject_template'),
                               $journal,
                               $this->get_publication_type_name())['result'];
 
