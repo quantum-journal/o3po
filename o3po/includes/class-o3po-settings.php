24ae7f4d91c9d35e2cb374cd476b9c858f7e8d5c
Christian Gogolin <git@cgogolin.de>
2019-04-25 01:07:40 +0200

    display more papers in the arxiv paper doi feed by default draw the feed url suffix and identifier form settings instead of hard coding them added documentation of this feature, addressing #93

--- o3po/includes/class-o3po-settings.php
+++ o3po/includes/class-o3po-settings.php
@@ -111,7 +111,7 @@ class O3PO_Settings extends O3PO_Singleton {
         'crossref_deposite_url' => 'https://doi.crossref.org/servlet/deposit',
         'crossref_test_deposite_url' => 'https://test.crossref.org/servlet/deposit',
         'clockss_ftp_url' => 'ftp.clockss.org',
-        'arxiv_doi_feed_identifier' => 'arxiv_paper_doi_feed',
+        'arxiv_doi_feed_identifier' => '',
         'arxiv_url_abs_prefix' => 'https://arxiv.org/abs/',
         'arxiv_url_pdf_prefix' => 'https://arxiv.org/pdf/',
         'arxiv_url_source_prefix' => 'https://arxiv.org/e-print/',
@@ -197,7 +197,7 @@ class O3PO_Settings extends O3PO_Singleton {
         'secondary_publication_type_name' => 'view',
         'secondary_publication_type_name_plural' => 'views',
         'volumes_endpoint' => 'volumes',
-
+        'arxiv_paper_doi_feed_endpoint' => 'arxiv_paper_doi_feed'
                                      );
 
         /**
@@ -347,6 +347,7 @@ class O3PO_Settings extends O3PO_Singleton {
         $this->add_settings_field('arxiv_url_source_prefix', 'Url prefix for eprint source', array( $this, 'render_arxiv_url_source_prefix_setting' ), $this->plugin_name . '-settings:arxiv_settings', 'arxiv_settings');
         $this->add_settings_field('arxiv_url_trackback_prefix', 'Url prefix for trackbacks', array( $this, 'render_arxiv_url_trackback_prefix_setting' ), $this->plugin_name . '-settings:arxiv_settings', 'arxiv_settings');
         $this->add_settings_field('arxiv_doi_feed_identifier', 'Indentifier for the DOI feed', array( $this, 'render_arxiv_doi_feed_identifier_setting' ), $this->plugin_name . '-settings:arxiv_settings', 'arxiv_settings');
+        $this->add_settings_field('arxiv_paper_doi_feed_endpoint', 'Endpoint for the arXiv DOI feed', array( $this, 'render_arxiv_paper_doi_feed_endpoint_setting' ), $this->plugin_name . '-settings:arxiv_settings', 'arxiv_settings');
 
         $this->add_settings_section('other_service_settings', 'Other services', array( $this, 'render_other_service_settings' ), $this->plugin_name . '-settings:other_service_settings');
         $this->add_settings_field('doi_url_prefix', 'Url prefix for DOI resolution', array( $this, 'render_doi_url_prefix_setting' ), $this->plugin_name . '-settings:other_service_settings', 'other_service_settings');
@@ -1039,6 +1040,22 @@ class O3PO_Settings extends O3PO_Singleton {
          */
     public function render_arxiv_doi_feed_identifier_setting() {
         $this->render_setting('arxiv_doi_feed_identifier');
+        echo '<p>(The arXiv can automatically update the journal reference of preprints based on information provided by publishers in the form of an xml feed as described <a href="https://arxiv.org/help/bib_feed">here</a>. For this to work, you must pick an identifier (specified via this setting) and inform the arXiv about the url under which the feed is available (can be configured in the following setting).)</p>';
+    }
+
+        /**
+         * Render the setting for the DOI feed identifier for the arXiv.
+         *
+         * @since    0.1.0
+         * @access   public
+         */
+    public function render_arxiv_paper_doi_feed_endpoint_setting() {
+        $this->render_setting('arxiv_paper_doi_feed_endpoint');
+        $settings = O3PO_Settings::instance();
+        $endpoint_suffix = $settings->get_plugin_option('arxiv_paper_doi_feed_endpoint');
+        $arxiv_paper_doi_feed_endpoint_url = get_site_url() . $endpoint_suffix;
+
+        echo '<p>(With the current setting the feed is available under <a href="' . esc_attr($arxiv_paper_doi_feed_endpoint_url) . '">' . esc_html($arxiv_paper_doi_feed_endpoint_url) . '</a>.)</p>';
     }
 
         /**
@@ -1300,6 +1317,7 @@ class O3PO_Settings extends O3PO_Singleton {
                 'author_notification_secondary_body_template' => 'trim_settings_field',
                 'fermats_library_notification_subject_template' => 'trim_settings_field',
                 'fermats_library_notification_body_template' => 'trim_settings_field',
+                'arxiv_paper_doi_feed_endpoint' => 'trim_settings_field_ensure_not_empty_and_flush_rewrite_rules_if_changed',
                                                    );
 
         return self::$all_settings_fields_map;
@@ -1428,6 +1446,33 @@ class O3PO_Settings extends O3PO_Singleton {
 
         add_settings_error( $field, 'not-checked-or-unchecked', "The field '" . $this->settings_fields[$field]['title'] . "' must be either checked or unchecked. Set to unchecked.", 'error');
         return 'unchecked';
+    }
+
+        /**
+         * Trim user input to settings
+         *
+         * @since    0.3.0
+         * @access   private
+         * @param    string   $field    The field this was input to.
+         * @param    string   $input    User input.
+         */
+    public function trim_settings_field_ensure_not_empty_and_flush_rewrite_rules_if_changed( $field, $input ) {
+        $input = trim($input);
+        if(empty($input))
+        {
+            add_settings_error( $field, 'must-not-be-empty', "The field '" . $this->settings_fields[$field]['title'] . "' must not be empty. Reset to default value.", 'error');
+            $input = $this->option_defaults[$field];
+        }
+        if(empty($input))
+            $input = 'this-field-must-not-be-empty';
+
+        if($input !== $this->get_plugin_option($field))
+        {
+            flush_rewrite_rules(true);
+            add_settings_error( $field, 'rewrite-rules-updated', "The rewrite rules have been updated because the field '" . $this->settings_fields[$field]['title'] . "' was changed.", 'updated');
+        }
+
+        return $input;
     }
 
         /**
