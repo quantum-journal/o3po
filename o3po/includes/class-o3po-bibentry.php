2c582d05ad913c87404dd66e1b3d7320828f50c7
Christian Gogolin <git@cgogolin.de>
2019-04-24 23:41:48 +0200

    removed all example.com with the correct url, this closes #101

--- o3po/includes/class-o3po-bibentry.php
+++ o3po/includes/class-o3po-bibentry.php
@@ -3,7 +3,7 @@
 /**
  * A class to represent bibliography entries.
  *
- * @link       http://example.com
+ * @link       https://quantum-journal.org/o3po/
  * @since      0.3.0
  *
  * @package    O3PO
