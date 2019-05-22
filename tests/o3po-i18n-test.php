43fa77836a4bd93e7d8c21f5a1e81236c440e271
Christian Gogolin <git@cgogolin.de>
2019-05-22 21:35:06 +0200

    added test of execute_load_plugin_textdomain()

---
+++ tests/o3po-i18n-test.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname( __FILE__ ) . '/../o3po/includes/class-o3po-i18n.php';
+
+class O3PO_I18nTest extends PHPUnit_Framework_TestCase
+{
+
+
+    public function test_execute_load_plugin_textdomain() {
+
+        $i18n = new O3PO_i18n();
+        $i18n->load_plugin_textdomain();
+
+    }
+
+}
