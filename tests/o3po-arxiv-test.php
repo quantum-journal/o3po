061d3df83501a9cafe5c243ca4cdaf2fdc39e531
Christian Gogolin <git@cgogolin.de>
2023-12-19 22:15:27 +0100

    get wp error message

--- tests/o3po-arxiv-test.php
+++ tests/o3po-arxiv-test.php
@@ -172,7 +172,7 @@ ERROR: No license informatin found on https://arxiv.org/abs/0809.2542v5.
             $this->assertEquals($expected, $submission_history);
         else
         {
-            $this->assertTrue(is_array($submission_history), "submission_history is not an array but has class " . get_class($submission_history));
+            $this->assertFalse(is_wp_error($submission_history), $submission_history->get_error_message());
             $this->assertEquals(
                 array_keys($expected),
                 array_keys($submission_history)
