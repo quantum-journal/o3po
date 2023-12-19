56eda4f9446c62194bdd9c74b2878cb9024b2591
Christian Gogolin <git@cgogolin.de>
2023-12-19 22:42:36 +0100

    fixed typo

--- o3po/includes/class-o3po-arxiv.php
+++ o3po/includes/class-o3po-arxiv.php
@@ -186,7 +186,7 @@ class O3PO_Arxiv {
                 preg_match('#\s*(?<date>[^[(]*) \((?<size>[0-9,.]* [kKmMgGbB]*)\)#u', $submission_history_date_size_info_nodes[$idx]->nodeValue, $match);
 
                 if(!in_array('date', $match))
-                    return new WP_Error('exception', "ERROR: did not find date in match " . json_encode($match) . " from " . $submission_history_date_size_info_nodes[$idx]->nodeValue . " with id" . $id);
+                    return new WP_Error('exception', "ERROR: did not find date in match " . json_encode($match) . " from " . $submission_history_date_size_info_nodes[$idx]->nodeValue . " with id" . $idx);
 
                 $submission_history[mb_substr($version_node->nodeValue, 1, -1)] = array(
                     'date' => strtotime($match['date']),
