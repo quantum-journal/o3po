62b3e083dba969024f2bb14248eb5c0fed8891d3
Christian Gogolin <git@cgogolin.de>
2025-01-01 23:53:14 +0100

    removed exception for debugging

--- o3po/includes/class-o3po-arxiv.php
+++ o3po/includes/class-o3po-arxiv.php
@@ -186,8 +186,8 @@ class O3PO_Arxiv {
             $submission_history = array();
             foreach($submission_history_version_nodes as $idx => $version_node)
             {
-                /* if(strlen(trim($submission_history_date_size_info_nodes[$idx]->nodeValue)) == 0) */
-                /*     continue; */
+                if(strlen(trim($submission_history_date_size_info_nodes[$idx]->nodeValue)) == 0)
+                    continue;
                 preg_match('#\s*(?<date>[^[(]*) \((?<size>[0-9,.]* [kKmMgGbB]*)\)#u', $submission_history_date_size_info_nodes[$idx]->nodeValue, $match);
 
                 if(!array_key_exists('date', $match) || !array_key_exists('size', $match))
@@ -199,7 +199,7 @@ class O3PO_Arxiv {
                     'comment' => '', # we currently only fetch and add the comment of the latest version below
                                                                                         );
             }
-            return new WP_Error('exception', "faefaef" . json_encode($submission_history));
+            # return new WP_Error('exception', "exception for debugging" . json_encode($submission_history));
 
             $comments_node = $x_path->query("/html/body//div[contains(@class, 'metatable')]//td[contains(@class, 'comments')]")[0];
             if($comments_node)
